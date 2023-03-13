90d24bf2271ef172dca8095bd022d53604c2dd54
dmaberlin1 <dmaberlin77@gmail.com>
2023-03-13 11:29:50 +0200

    feat:  some components -Footer -Header -Sidebar

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -11,23 +11,51 @@ const Header = () => {
     return (
       <div className={styles.header}>
         <div className={styles.logo}>
-            <Link to={ROUTES.HOME}>
-                <img src={logo} alt="stuff"/>
+            <Link draggable={'false'} to={ROUTES.HOME}>
+                <img draggable={'false'} src={logo} alt="stuff"/>
             </Link>
         </div>
           <div className={styles.info}>
-              <div className="styles.user">
+              <div className={styles.user}>
                   <div className="styles.avatar" style={{backgroundImage:`url(${avatar})`}}></div>
                   <div className="styles.username">Guest</div>
               </div>
               <form className={styles.form}>
-                  <div className="styles.icon">
-
+                  <div className={styles.icon}>
+        <svg className="icon">
+            <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#search`}></use>
+        </svg>
                   </div>
+                  <div className={styles.input}>
+                      <input type="search"
+                             name={'search'}
+                             placeholder={'Search for anything'}
+                             autoComplete={'off'}
+                             onChange={()=>{}}
+                             value={''}
+                      />
+                  </div>
+
+                  {false && <div className={styles.box}></div>}
               </form>
+              <div className={styles.account}>
+                  <Link draggable={'false'} to={ROUTES.HOME} className={styles.favourites}>
+                      <svg className={styles["icon-fav"]}>
+                          <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#heart`}></use>
+                      </svg>
+                  </Link>
+                  <Link draggable={'false'} to={ROUTES.CART} className={styles.cart}>
+                      <svg className={styles["icon-cart"]}>
+                          <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#bag`}></use>
+                      </svg>
+                      <span className={styles.count}>2</span>
+                  </Link>
+              </div>
           </div>
       </div>
     );
 };
+
+
 
 export default Header;
--- src/component/Sidebar/Sidebar.jsx
+++ src/component/Sidebar/Sidebar.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import styles from '../../styles/Sidebar.module.css'
+import {NavLink} from "react-router-dom";
 
 const Sidebar = () => {
     return (
-      <div>
+      <section className={styles.sidebar}>
+            <div className={styles.title}>CATEGORIES</div>
+          <nav>
+              <ul className={styles.menu}>
+                  <li>
+                      <NavLink to={`/categories/${1}`}>Link</NavLink>
+                  </li>
+              </ul>
+          </nav>
 
-      </div>
+          <div className={styles.footer}>
+              <a draggable={'false'} target={'_blank'} href="/help" className={styles.link}>Help</a>
+              <a draggable={'false'}
+                 target={'_blank'} href="/terms"
+                 className={styles.link}
+                 style={{textDecoration:'underline'}}
+              >Terms & Conditions</a>
+          </div>
+      </section>
     );
 };
 
