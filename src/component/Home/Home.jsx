1c1413ef399b43bb24d7dd72c4d5707b04a656a6
dmaberlin1 <dmaberlin77@gmail.com>
2023-03-13 18:53:41 +0200

    feat: working with API

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import Poster from "../Poster/Poster";
-
+import Products from "../Products/Products";
+import {useSelector} from "react-redux";
+import Categories from "../Categories/Categories";
+const titles={
+    Products:'Trending',
+    Categories:'Worth seeing'
+}
 const Home = () => {
+    const {products,categories}=useSelector((state)=>state)
     return (
       <>
        <Poster></Poster>
+          <Products products={products.list} amount={5} title={titles.Products}></Products>
+          <Categories products={categories.list} amount={5} title={titles.Categories}></Categories>
       </>
     );
 };
--- src/component/Products/Products.jsx
+++ src/component/Products/Products.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
 
-const Products = () => {
-    return (
-      <div>
+import styles from '../../styles/Products.module.css'
+import {Link} from "react-router-dom";
 
-      </div>
+const Products = (props) => {
+    const {title,style={}, products = [],amount} = props
+    const list=products.filter((_,i)=>i<amount)
+    console.log(list.images)
+
+    return (
+      <section className={styles.products} style={style}>
+          {title && <h2>{title}</h2>}
+          <div className={styles.list}>
+              {list.map(({id, images, title, category: {name: cat}, price}) => (
+                <Link
+                  draggable={'false'}
+                  className={styles.product}
+                  to={`/products/${id}`}
+                  key={id}>
+                    <div className={styles.image}
+                         style={{backgroundImage:`url(${images[0]})`}}>
+                    </div>
+
+                    <div className={styles.wrapper}>
+                        <h3 className={styles.title}>{title}</h3>
+                        <div className={styles.cat}>{cat}</div>
+                        <div className={styles.info}>
+                            <div className={styles.price}>{price}</div>
+                            {price>0? <div className={styles.oldPrice}>{Math.floor(price*1.3)}$</div>:null }
+                        </div>
+
+                        <div className={styles.purchases}>{Math.floor(Math.random()*20+1)} purchased</div>
+                    </div>
+                </Link>
+              ))}
+          </div>
+      </section>
     );
 };
 
